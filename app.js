fc6086c6c0f085e488c9e1c624b9d74826ff1a58
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-02 23:48:06 +0200

    variables, check if browser supports geolocation

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const tempElement = document.querySelector('.temperature-value p');
 const descElement = document.querySelector('.temperature-description p');
 const locationElement = document.querySelector('.location p');
 const notificationElement = document.querySelector('.notification');
+
+// app data
+const weather = {};
+// define temperature unit
+weather.temperature = {
+  unit: "celsius"
+}
+
+// variables
+const KELVIN = 273;
+const apiKey = "46098c7949a688b482ae72bccedf24a3";
+
+// check if the browser supports geolocation
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = "<p>Browser doesn't support Geolocation </p>";
+}
