85794b7eb4749f01e8ead81a973de5a41a4d294b
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-02 23:37:56 +0200

    select elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// select the elements
+const iconElement = document.querySelector('.weather-icon');
+const tempElement = document.querySelector('.temperature-value p');
+const descElement = document.querySelector('.temperature-description p');
+const locationElement = document.querySelector('.location p');
+const notificationElement = document.querySelector('.notification');
